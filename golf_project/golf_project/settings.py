ee929ff8d02be29b5ac4f269aa84ea9c4228db9b
Rony George <ronygeorgengit@gmail.com>
2026-06-25 17:55:55 +0530

    added X-Forwarded-Proto and X-Forwarded-Host for display the logo in settings.py

--- golf_project/golf_project/settings.py
+++ golf_project/golf_project/settings.py
@@ -65,6 +65,11 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+# Trust proxy headers for absolute URIs
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+USE_X_FORWARDED_HOST = True
+
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
